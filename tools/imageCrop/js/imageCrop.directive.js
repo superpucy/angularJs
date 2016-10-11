b0d460651fcccc9f9439594ebfee1eb3173504b4
ina <ina.lai@hyweb.com.tw>
2016-10-11 17:48:06 +0800

    圖片剪裁:改用templateUrl

--- tools/imageCrop/js/imageCrop.directive.js
+++ tools/imageCrop/js/imageCrop.directive.js
@@ -3,12 +3,6 @@ app=angular.module("app",[]);
 app.directive("imageCropper",function(){
     return {
         restrict: "E",
-        template: '<div>'+
-            '<input type="file" id="file" name="file" accept="image/*" />'+
-             '<div ">'+
-                    '<input type="button" value="上傳"  />'+
-                    '<input type="submit" value="取消" />'+
-                 '</div>' +
-              '</div>'
+        templateUrl: 'imageCrop.html'
     };
 });
